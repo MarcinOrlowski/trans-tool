f0635a75af3093af6e2ff9176994c0ba34cbaf66
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-07-17 23:32:05 +0200

    Updated file header

--- proptool/const.py
+++ proptool/const.py
@@ -2,8 +2,8 @@
 # prop-tool
 # Java *.properties file sync checker and syncing tool.
 #
-# copyright ©2021 Marcin Orlowski <mail [@] MarcinOrlowski.com>
-# https://github.com/MarcinOrlowski/prop-tool
+# Copyright ©2021 Marcin Orlowski <mail [@] MarcinOrlowski.com>
+# https://github.com/MarcinOrlowski/prop-tool/
 #
 
 class Const:
