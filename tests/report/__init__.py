2ce99590ee1f82ad01d4109a7f9a98a4d5358a9d
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-07-21 13:29:10 +0200

    Moved Report* tests to subpackage

---
+++ tests/report/__init__.py
@@ -0,0 +1,8 @@
+"""
+# prop-tool
+# Java *.properties file sync checker and syncing tool.
+#
+# Copyright ©2021 Marcin Orlowski <mail [@] MarcinOrlowski.com>
+# https://github.com/MarcinOrlowski/prop-tool/
+#
+"""
