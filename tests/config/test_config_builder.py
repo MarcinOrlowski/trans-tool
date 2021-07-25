0666d12e26c179ad251561fce5e66ce707fd43a9
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-07-25 18:55:21 +0200

    Added some ConfigBuilder() tests.

---
+++ tests/config/test_config_builder.py
@@ -0,0 +1,63 @@
+"""
+# prop-tool
+# Java *.properties file sync checker and syncing tool.
+#
+# Copyright ©2021 Marcin Orlowski <mail [@] MarcinOrlowski.com>
+# https://github.com/MarcinOrlowski/prop-tool/
+#
+"""
+from pathlib import Path
+from unittest.mock import call, patch
+
+from proptool.config.config import Config
+from proptool.config.config_builder import ConfigBuilder
+from tests.test_case import TestCase
+
+
+class TestConfigBuilder(TestCase):
+
+    def get_config_for_validate(self) -> Config:
+        config = Config()
+
+        config.files = [Path('/tmp/')]
+        config.languages = ['pl']
+        config.separator = Config.ALLOWED_SEPARATORS[0]
+        config.comment_marker = Config.ALLOWED_COMMENT_MARKERS[0]
+
+        return config
+
+    def test_validate(self) -> None:
+        # This one should pass
+        ConfigBuilder._validate(self.get_config_for_validate())
+
+    @patch('proptool.log.Log.abort')
+    def test_validate_no_files(self, mock_log_abort) -> None:
+        config = self.get_config_for_validate()
+        config.files = []
+        ConfigBuilder._validate(config)
+        exp_calls = [call('No base file(s) specified.')]
+        mock_log_abort.assert_has_calls(exp_calls)
+
+    @patch('proptool.log.Log.abort')
+    def test_validate_no_languages(self, mock_print) -> None:
+        config = self.get_config_for_validate()
+        config.languages = []
+        ConfigBuilder._validate(config)
+        exp_calls = [call('No language(s) specified.')]
+        mock_print.assert_has_calls(exp_calls)
+
+    @patch('proptool.log.Log.abort')
+    def test_validate_invalid_separator(self, mock_print) -> None:
+        config = self.get_config_for_validate()
+        config.separator = ''
+        ConfigBuilder._validate(config)
+        exp_calls = [call('Invalid separator character.')]
+        mock_print.assert_has_calls(exp_calls)
+
+    @patch('proptool.log.Log.abort')
+    def test_validate_invalid_comment_marker(self, mock_print) -> None:
+        config = self.get_config_for_validate()
+        config.comment_marker = ''
+        ConfigBuilder._validate(config)
+        exp_prints = [call('Invalid comment marker.')]
+        mock_print.assert_has_calls(exp_prints)
