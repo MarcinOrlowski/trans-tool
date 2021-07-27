4a49d23e54e59f9434690f87b18edec7e6e72b89
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-07-27 14:00:58 +0200

    Added base Check class tests

--- tests/checks/test_base_check.py
+++ tests/checks/test_base_check.py
@@ -53,6 +53,35 @@ class TestBaseCheck(TestCase):
         with self.assertRaises(NotImplementedError):
             check.check(None, None)
 
+    def test_need_valid_config_valid_arg(self) -> None:
+        config = Config()
+        # Check is abstract class, extending ABC meta, so ordinary
+        # instantiation won't work.
+        Check.__abstractmethods__ = frozenset()
+        check = Check(config)
+        # We expect no problems.
+        check.need_valid_config()
+
+    def test_need_valid_config_no_config(self) -> None:
+        # Check is abstract class, extending ABC meta, so ordinary
+        # instantiation won't work.
+        Check.__abstractmethods__ = frozenset()
+        check = Check()
+        # Exception should be thrown
+        with self.assertRaises(ValueError):
+            check.need_valid_config()
+
+    def test_need_valid_config_invalid_config_type(self) -> None:
+        # Check is abstract class, extending ABC meta, so ordinary
+        # instantiation won't work.
+        Check.__abstractmethods__ = frozenset()
+        check = Check()
+        # Inject faulty 'config' to walk around constructor check.
+        check.config = 123
+        # Exception should be thrown
+        with self.assertRaises(ValueError):
+            check.need_valid_config()
+
     def test_need_both_files_missing_file(self) -> None:
         """
         Checks how need_both_files() is handling missing arguments.
