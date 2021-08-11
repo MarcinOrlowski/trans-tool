474a5cc46e14c9369bfba23eea08ce5935f27bf5
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-08-11 11:10:46 +0200

    Fixed pypi's README to use absolute image URLs

--- setup.py
+++ setup.py
@@ -22,43 +22,43 @@ from transtool.const import Const
 from setuptools import setup, find_packages
 
 with open('README.md', 'r') as fh:
-    readme = fh.read()
-    readme.replace('![trans-tool logo](artwork/trans-tool-logo.png)',
-                   'https://raw.githubusercontent.com/MarcinOrlowski/trans-tool/master/artwork/trans-tool-logo.png', 1)
+    logo_url = 'https://raw.githubusercontent.com/MarcinOrlowski/trans-tool/master/artwork/trans-tool-logo.png'
+    readme = fh.read().replace(r'![trans-tool logo](artwork/trans-tool-logo.png)',
+                               f'![trans-tool logo]({logo_url})', 1)
 
-setup(
-    name = Const.APP_NAME,
-    version = Const.APP_VERSION,
-    packages = find_packages(),
+    setup(
+        name = Const.APP_NAME,
+        version = Const.APP_VERSION,
+        packages = find_packages(),
 
-    install_requires = [
-        'argparse>=1.4.0',
-    ],
-    python_requires = '>=3.6',
-    entry_points = {
-        'console_scripts': [
-            'trans-tool = transtool.main:TransTool.start',
-            'transtool = transtool.main:TransTool.start',
+        install_requires = [
+            'argparse>=1.4.0',
         ],
-    },
+        python_requires = '>=3.6',
+        entry_points = {
+            'console_scripts': [
+                'trans-tool = transtool.main:TransTool.start',
+                'transtool = transtool.main:TransTool.start',
+            ],
+        },
 
-    author = 'Marcin Orlowski',
-    author_email = 'mail@marcinOrlowski.com',
-    description = 'The translation files checker and syncing tool.',
-    long_description = readme,
-    long_description_content_type = 'text/markdown',
-    url = Const.APP_URL,
-    keywords = 'translation helper locale language sync check validation',
-    project_urls = {
-        'Bug Tracker':   'https://github.com/MarcinOrlowski/trans-tool/issues/',
-        'Documentation': 'https://github.com/MarcinOrlowski/trans-tool/',
-        'Source Code':   'https://github.com/MarcinOrlowski/trans-tool/',
-    },
-    # https://choosealicense.com/
-    license = 'MIT License',
-    classifiers = [
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-)
+        author = 'Marcin Orlowski',
+        author_email = 'mail@marcinOrlowski.com',
+        description = 'The translation files checker and syncing tool.',
+        long_description = readme,
+        long_description_content_type = 'text/markdown',
+        url = Const.APP_URL,
+        keywords = 'translation helper locale language sync check validation',
+        project_urls = {
+            'Bug Tracker':   'https://github.com/MarcinOrlowski/trans-tool/issues/',
+            'Documentation': 'https://github.com/MarcinOrlowski/trans-tool/',
+            'Source Code':   'https://github.com/MarcinOrlowski/trans-tool/',
+        },
+        # https://choosealicense.com/
+        license = 'MIT License',
+        classifiers = [
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+        ],
+    )
