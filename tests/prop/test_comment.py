2945a1476d06380e83167919e6af83694dcb6ed3
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2023-10-23 20:12:22 +0200

    Updated code headers

--- tests/prop/test_comment.py
+++ tests/prop/test_comment.py
@@ -1,11 +1,10 @@
-"""
+#
 # trans-tool
 # The translation files checker and syncing tool.
 #
-# Copyright ©2021 Marcin Orlowski <mail [@] MarcinOrlowski.com>
+# Copyright ©2021-2023 Marcin Orlowski <mail [@] MarcinOrlowski.com>
 # https://github.com/MarcinOrlowski/trans-tool/
 #
-"""
 
 from transtool.config.config import Config
 from transtool.prop.items import Comment
